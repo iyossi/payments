8e52c9e32524a805b2622ce71f0604a1ebb21f42
iyossi <iyossi@gmail.com>
2018-12-23 00:15:51 +0200

    more REST green path

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -9,14 +9,5 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-//        String[] beanNames = ctx.getBeanDefinitionNames();
-//
-//        Arrays.sort(beanNames);
-//
-//        for (String beanName : beanNames)
-//        {
-//            System.out.println(beanName);
-//        }
     }
 }
