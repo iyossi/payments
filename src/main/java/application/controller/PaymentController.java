0593ec4a368cd18879af2c9f86b50084bc6e3f35
iyossi <iyossi@gmail.com>
2018-12-27 01:20:38 +0200

    Payee, methods and Exceptions

--- src/main/java/application/controller/PaymentController.java
+++ src/main/java/application/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package application.controller;
 
+import application.Exception.BasicRestException;
 import application.model.RequestDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,11 @@ public class PaymentController {
 
     @PostMapping(path = "/payments")
     public UUID createPayment(@RequestBody RequestDto request) {
-        System.out.println("request is " + request.toString());
-        return UUID.randomUUID();
+        try {
+            System.out.println("request is " + request.toString());
+            return UUID.randomUUID();
+        } catch (RuntimeException e) {
+            throw new BasicRestException("Can't get customers");
+        }
     }
 }
