3835aaabaf190084d55e5cc045884456e7c6906c
iyossi <iyossi@gmail.com>
2018-12-30 01:38:07 +0200

    msg sent and risk evaluated but not yet used

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,6 +1,8 @@
 package application;
 
-import application.msgHandlers.RiskEngineMsgConsumer;
+import application.service.RiskEngineService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
@@ -9,7 +11,6 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,29 +18,24 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-    public static String getTopicExchangeName() {
-        return topicExchangeName;
-    }
 
-    public static String getQueueName() {
-        return QUEUE_NAME;
-    }
+    static Logger logger = LoggerFactory.getLogger(Application.class);
 
     static final String topicExchangeName = "riskEngine-exchange";
 
     static final public String QUEUE_NAME = "paymentRequests";
 
+    public static String getTopicExchangeName() {
+        return topicExchangeName;
+    }
+
+
     @Bean
     public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-//        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
         return rabbitTemplate;
     }
 
-    @Bean
-    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
 
     @Bean
     Queue queue() {
@@ -53,7 +49,7 @@ public class Application {
 
     @Bean
     Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+        return BindingBuilder.bind(queue).to(exchange).with("payments.requests.#");
     }
 
     @Bean
@@ -67,14 +63,14 @@ public class Application {
     }
 
     @Bean
-    MessageListenerAdapter listenerAdapter(RiskEngineMsgConsumer consumer) {
+    MessageListenerAdapter listenerAdapter(RiskEngineService consumer) {
         return new MessageListenerAdapter(consumer, "consumeMessage");
     }
 
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
+        logger.info("Starting");
     }
 
 
